b6b66ac985deed5c45cf153336fb3a355d6019fd
Lickme404 <102676188+Lickme404@users.noreply.github.com>
2022-03-31 06:52:36 +0700

    Add files via upload

---
+++ LOGIN.py
@@ -0,0 +1,44 @@
+#coding: utf-8
+# module
+import os,sys,time,getpass
+
+# username
+x = "randi"
+# password
+y = "asd"
+
+
+# mengetik otomatis cepat
+def sp(a):
+  for e in a + "\n":
+    sys.stdout.write(e)
+    sys.stdout.flush()
+    time.sleep(0.002)
+
+
+# login
+def login():
+  os.system("clear")
+  sp("\033[1;96m       ╔╗   ╔═══╗╔═══╗╔══╗╔═╗ ╔╗")
+  sp("       ║║   ║╔═╗║║╔═╗║╚╣─╝║║╚╗║║")
+  sp("       ║║   ║║ ║║║║ ╚╝ ║║ ║╔╗╚╝║")
+  sp("       ║║ ╔╗║║ ║║║║╔═╗ ║║ ║║╚╗║║")
+  sp("       ║╚═╝║║╚═╝║║╚╩═║╔╣─╗║║ ║║║")
+  sp("       ╚═══╝╚═══╝╚═══╝╚══╝╚╝ ╚═╝")
+  sp("\033[1;96m+\033[1;90m========================================\033[1;96m+")
+  sp(" \033[1;97m{\033[1;95m•\033[1;97m} \033[1;93mAuthor   \033[1;91m: \033[1;95mRandiansyah")
+  sp(" \033[1;97m{\033[1;95m•\033[1;97m} \033[1;93mFacebook \033[1;91m: \033[1;95mRendi Kun")
+  sp(" \033[1;97m{\033[1;95m•\033[1;97m} \033[1;93mYouTube  \033[1;91m: \033[1;95mRendi Noober")
+  sp(" \033[1;97m{\033[1;95m•\033[1;97m} \033[1;93mGithub   \033[1;91m: \033[4;92mhttps://github.com/Rendi-ID\033[1;0m")
+  sp("\033[1;96m+\033[1;90m========================================\033[1;96m+")
+  username = raw_input(" \033[1;97m{\033[1;93m+\033[1;97m} \033[1;96mUsername:\033[1;92m ")
+  password = getpass.getpass(" \033[1;97m{\033[1;93m+\033[1;97m} \033[1;96mPassword: ")
+  if username == x and password == y:
+    print(" \033[1;92mAccess Grented")
+    time.sleep(1)
+  else:
+    print(" \033[1;91mAccess Denied")
+    time.sleep(1)
+    login()
+
+login()
